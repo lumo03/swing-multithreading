35c87d25d041bbc9d8aee935967eb4fb08474f67
lumo03 <luis.moncada.mso@gmail.com>
2023-02-09 17:16:01 +0100

    setup project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Luis Moncada (lumo03)
+ *
+ */
 public class Main {
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 	}
+
 }
